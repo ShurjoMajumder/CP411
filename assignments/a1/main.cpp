a4a4720f8d862d570c27bfeb611a13607c5c5b46
Shurjo Majumder <shurjo.majumder@gmail.com>
2024-09-14 14:37:29 -0400

    GLUT now works.

--- assignments/a1/main.cpp
+++ assignments/a1/main.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
+#include "GL/glew.h"
+#include "GL/glut.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void init();
+void display();
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(400, 300);
+    glutCreateWindow("test");
+    init();
+
+    //Call "display" function
+    glutDisplayFunc(display);
+
+    //Enter the GLUT event loop
+    glutMainLoop();
+
     return 0;
+}
+
+void init() {
+    //select clearing (background) color
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+
+    //initialize viewing values
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+}
+
+void display() {
+    //Clear all pixels
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    //draw white polygon (rectangle) with corners at
+    // (0.25, 0.25, 0.0) and (0.75, 0.75, 0.0)
+    glColor3f(1.0,1.0,1.0);
+    glBegin(GL_POLYGON);
+    glVertex3f(0.25, 0.25, 0.0);
+    glVertex3f(0.75, 0.25, 0.0);
+    glVertex3f(0.75, 0.75, 0.0);
+    glVertex3f(0.25, 0.75, 0.0);
+    glEnd();
+
+    // Don't wait start processing buffered OpenGL routines
+    glFlush();
 }
